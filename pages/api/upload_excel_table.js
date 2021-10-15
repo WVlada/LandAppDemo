e059f83bb85c052f7de5c29a7f9c43fcc228ee78
Vladimir Bukvić <vladimir.bukvic@live.com>
2021-10-15 16:23:19 +0200

    f

--- pages/api/upload_excel_table.js
+++ pages/api/upload_excel_table.js
@@ -11,44 +11,44 @@ export default async function handler(req, res) {
   //  }
   //form.uploadDir = "./utils/upload/";
   //form.keepExtensions = true;
-  form.parse(req, async (err, fields, files) => {
-    //console.log(err, fields, files);
-    const ExcelJS = require("exceljs");
-    const workbook = new ExcelJS.Workbook();
-    await workbook.xlsx.readFile(files.excel_table.path);
-    const worksheet = workbook.getWorksheet("sve");
-    const rowCount = worksheet.rowCount;
-    console.log(rowCount);
-    const prisma = new PrismaClient();
-    let deletedParcels = await prisma.parcel.deleteMany({});
-    try {
-      let rb = 1;
-      for (let i = 2; i <= rowCount; i++) {
-        let n = worksheet.getRow(i).findCell(5)
-          ? worksheet.getRow(i).findCell(5).text
-          : "*";
-        //console.log(n);
-        let parcel = await prisma.parcel.create({
-          data: {
-            rb: rb,
-            ko: worksheet.getRow(i).findCell(3).text,
-            potes: worksheet.getRow(i).findCell(4)
-              ? worksheet.getRow(i).findCell(4).text
-              : "",
-            number: worksheet.getRow(i).findCell(5)
-              ? worksheet.getRow(i).findCell(5).text
-              : "",
-            klasa: worksheet.getRow(i).findCell(6).text,
-            povrsina: parseInt(worksheet.getRow(i).findCell(7).text),
-            vlasnistvo: worksheet.getRow(i).findCell(9).text,
-          },
-        });
-        rb += 1;
-      }
-    } catch (err) {
-      return res.status(500).json({ text: "Error with some excel fields" });
-    }
-  });
+  //form.parse(req, async (err, fields, files) => {
+  //  //console.log(err, fields, files);
+  //  const ExcelJS = require("exceljs");
+  //  const workbook = new ExcelJS.Workbook();
+  //  await workbook.xlsx.readFile(files.excel_table.path);
+  //  const worksheet = workbook.getWorksheet("sve");
+  //  const rowCount = worksheet.rowCount;
+  //  console.log(rowCount);
+  //  const prisma = new PrismaClient();
+  //  let deletedParcels = await prisma.parcel.deleteMany({});
+  //  try {
+  //    let rb = 1;
+  //    for (let i = 2; i <= rowCount; i++) {
+  //      let n = worksheet.getRow(i).findCell(5)
+  //        ? worksheet.getRow(i).findCell(5).text
+  //        : "*";
+  //      //console.log(n);
+  //      let parcel = await prisma.parcel.create({
+  //        data: {
+  //          rb: rb,
+  //          ko: worksheet.getRow(i).findCell(3).text,
+  //          potes: worksheet.getRow(i).findCell(4)
+  //            ? worksheet.getRow(i).findCell(4).text
+  //            : "",
+  //          number: worksheet.getRow(i).findCell(5)
+  //            ? worksheet.getRow(i).findCell(5).text
+  //            : "",
+  //          klasa: worksheet.getRow(i).findCell(6).text,
+  //          povrsina: parseInt(worksheet.getRow(i).findCell(7).text),
+  //          vlasnistvo: worksheet.getRow(i).findCell(9).text,
+  //        },
+  //      });
+  //      rb += 1;
+  //    }
+  //  } catch (err) {
+  //    return res.status(500).json({ text: "Error with some excel fields" });
+  //  }
+  //});
   // now remove all files
   //const directory = "./utils/upload/";
 //
