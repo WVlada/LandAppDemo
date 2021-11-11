4a455292a080c169adbb79ed5d23edeb78e401ea
Vladimir Bukvić <vladimir.bukvic@live.com>
2021-11-11 13:05:44 +0100

    17

--- pages/opstina/[name].js
+++ pages/opstina/[name].js
@@ -17,80 +17,125 @@ import Link from "next/link";
 import { formatNumber, makeOpstineFromFirme } from "../../utils/utils";
 import Map from "../../components/index/map_component";
 
-export default function Firma({ opstina, parcelsJSON, hipoteke, opstine }) {
+export default function Firma({
+  opstina,
+  parcelsJSON,
+  hipoteke,
+  opstine,
+  koJSON,
+}) {
   const parcels = JSON.parse(parcelsJSON);
+  const ko = JSON.parse(koJSON);
 
   return (
-    <div className="flex flex-col flex-1">
+    <div className="flex flex-col flex-1 mb-10">
       {<Map opstine={opstine} />}
-      <Table
-        className="text-xs md:text-lg"
-        variant="striped"
-        colorScheme="gray"
-        mt={[2, 10]}
-      >
-        <TableCaption placement="top">
-          Pregled zemljišta u opštini {opstina}
-        </TableCaption>
-        <Thead>
-          <Tr>
-            <Th textAlign="center" p={[1, 5]}>
-              Vlasnik
-            </Th>
-            <Th textAlign="center" p={[1, 5]}>
-              Površina
-            </Th>
-            <Th textAlign="center" p={[1, 5]}>
-              Hipoteka
-            </Th>
-            <Th textAlign="center" p={[1, 5]}>
-              Slobodno
-            </Th>
-          </Tr>
-        </Thead>
-        <Tbody>
-          {parcels.map((parcel, index) => {
-            return (
-              <Tr className="cursor-pointer" key={index}>
-                <Td p={[1, 5]}>{parcel.opstina}</Td>
-                <Td textAlign="right" p={[1, 5]}>
-                  {formatNumber(parcel.povrsina)}
-                </Td>
-                <Td p={[1, 5]} isNumeric>
-                  {formatNumber(parcel.povrsina)}
-                </Td>
-                <Td p={[1, 5]} isNumeric>
-                  {formatNumber(parcel.povrsina)}
-                </Td>
+      {Object.keys(ko).map((firma, index) => {
+        return (
+          <Table
+            className="text-xs md:text-lg"
+            variant="striped"
+            colorScheme="gray"
+            mt={[2, 10]}
+            key={index}
+          >
+            <TableCaption placement="top">
+              Pregled zemljišta: {firma}
+            </TableCaption>
+            <Thead>
+              <Tr>
+                <Th textAlign="center" p={[1, 5]}>
+                  Katastarska opština
+                </Th>
+                <Th textAlign="center" p={[1, 5]}>
+                  Površina
+                </Th>
+                <Th textAlign="center" p={[1, 5]}>
+                  Hipoteka
+                </Th>
+                <Th textAlign="center" p={[1, 5]}>
+                  Slobodno
+                </Th>
               </Tr>
-            );
-          })}
-        </Tbody>
-        <Tfoot>
-          <Tr>
-            <Th textAlign="center" p={[1, 5]}>
-              <p className="lowercase font-extrabold text-xs md:text-lg">
-                {formatNumber(0)}
-              </p>
-            </Th>
-            <Th textAlign="center" p={[1, 5]}>
-              <p className="lowercase font-extrabold text-xs md:text-lg">
-                {formatNumber(0)}
-              </p>
-            </Th>
-            <Th textAlign="center" p={[1, 5]}>
-              <p className="lowercase font-extrabold text-xs md:text-lg">
-                {formatNumber(0)}
-              </p>
-            </Th>
-            <Th textAlign="center" p={[1, 5]}>
-              <p className="lowercase font-extrabold text-xs md:text-lg">
-                {formatNumber(0)}
-              </p>
-            </Th>
-          </Tr>
-        </Tfoot>
-      </Table>
+            </Thead>
+            <Tbody>
+              {Object.keys(ko[firma]).map((kopstina, index) => {
+                return (
+                  <Tr className="cursor-pointer" key={index}>
+                    <Td p={[1, 5]}>{kopstina}</Td>
+                    <Td textAlign="right" p={[1, 5]}>
+                      {formatNumber(
+                        ko[firma][kopstina].reduce((a, b) => a + b.povrsina, 0)
+                      )}
+                    </Td>
+                    <Td p={[1, 5]} isNumeric>
+                      {formatNumber( hipoteke[firma] &&
+                        hipoteke[firma][kopstina]
+                          ? hipoteke[firma][kopstina]
+                          : 0
+                      )}
+                    </Td>
+                    <Td p={[1, 5]} isNumeric>
+                      {formatNumber(
+                        ko[firma][kopstina].reduce(
+                          (a, b) => a + b.povrsina,
+                          0
+                        ) -
+                          (hipoteke[firma] && hipoteke[firma][kopstina]
+                            ? hipoteke[firma][kopstina]
+                            : 0)
+                      )}
+                    </Td>
+                  </Tr>
+                );
+              })}
+            </Tbody>
+            <Tfoot>
+              <Tr>
+                <Th textAlign="center" p={[1, 5]}>
+                  <p className="lowercase font-extrabold text-xs md:text-lg">
+                    {}
+                  </p>
+                </Th>
+                <Th textAlign="center" p={[1, 5]}>
+                  <p className="lowercase font-extrabold text-xs md:text-lg">
+                    {formatNumber(
+                      parcels.filter((p) => p._id == firma)[0]["sum"]
+                    )}
+                  </p>
+                </Th>
+                <Th textAlign="center" p={[1, 5]}>
+                  <p className="lowercase font-extrabold text-xs md:text-lg">
+                    {formatNumber(
+                      hipoteke[firma] ? 
+                      Object.keys(hipoteke[firma])
+                        .map((e) => {
+                          return hipoteke[firma][e];
+                        })
+                        .reduce((a, b) => a + b, 0) 
+                        : 0
+                    )}
+                  </p>
+                </Th>
+                <Th textAlign="center" p={[1, 5]}>
+                  <p className="lowercase font-extrabold text-xs md:text-lg">
+                    {formatNumber(
+                      parcels.filter((p) => p._id == firma)[0]["sum"] -
+                        (hipoteke[firma]
+                          ? Object.keys(hipoteke[firma])
+                              .map((e) => {
+                                return hipoteke[firma][e];
+                              })
+                              .reduce((a, b) => a + b, 0)
+                          : 0)
+                    )}
+                  </p>
+                </Th>
+              </Tr>
+            </Tfoot>
+          </Table>
+        );
+      })}
     </div>
   );
 }
@@ -117,10 +162,11 @@ export async function getServerSideProps(context) {
         ko[red.vlasnistvo][red.ko] = [red];
       }
     } else {
-      ko[red.vlasnistvo] = {}
+      ko[red.vlasnistvo] = {};
+      ko[red.vlasnistvo][red.ko] = [red];
     }
   });
-  console.log("ko", ko);
+  //console.log("ko", ko);
   //let vlasnistvoSum = await Parcel.aggregate([
   //  { $group: { _id: "$vlasnistvo", sum: { $sum: "$povrsina" } } },
   //  { $sort: { _id: -1 } },
@@ -132,7 +178,7 @@ export async function getServerSideProps(context) {
   parcels.map((e) => {
     firme[e._id] = { active: true };
   });
-  //console.log("parcels", parcels);
+  console.log("parcels", parcels);
   //const sum = parcels.reduce(function (a, b) {
   //  return a + b.povrsina;
   //}, 0);
@@ -149,22 +195,33 @@ export async function getServerSideProps(context) {
       hipoteka_1: "$hipoteka_1",
       hipoteka_2: "$hipoteka_2",
       vlasnistvo: "$vlasnistvo",
+      ko: "$ko",
     },
     sum: { $sum: "$povrsina" },
   });
-  //console.log("hip:", hip);
+  console.log("hip:", hip[0]);
   let hipoteke = {};
   hip.map((red) => {
     if (red._id.hipoteka_1 || red._id.hipoteka_2) {
       if (hipoteke[red._id.vlasnistvo]) {
-        hipoteke[red._id.vlasnistvo] += red.sum;
+        if (hipoteke[red._id.vlasnistvo][red._id.ko]) {
+          hipoteke[red._id.vlasnistvo][red._id.ko] += red.sum;
+        } else {
+          hipoteke[red._id.vlasnistvo][red._id.ko] = red.sum;
+        }
       } else {
-        hipoteke[red._id.vlasnistvo] = red.sum;
+        hipoteke[red._id.vlasnistvo] = {};
+        hipoteke[red._id.vlasnistvo][red._id.ko] = red.sum;
+
+        //hipoteke[red._id.vlasnistvo][red._id.ko] = red.sum;
       }
     } else {
     }
   });
   console.log("hipteke", hipoteke);
+
+  //ko["Vanja N."]["Banatski brestovac"].map((a) => console.log(a.broj_parcele));
+
   let opstinePocetno = await Parcel.aggregate([
     { $match: { opstina: name } },
     {
@@ -181,6 +238,7 @@ export async function getServerSideProps(context) {
       parcelsJSON: JSON.stringify(parcels),
       hipoteke: hipoteke,
       opstine: opstineSrednjeno,
+      koJSON: JSON.stringify(ko),
     },
   };
 }
