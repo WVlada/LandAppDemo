f1e179850af9243bd706623f4c446d45fc0f0513
Vladimir Bukvić <vladimir.bukvic@live.com>
2021-11-15 18:34:00 +0100

    3

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,27 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
   }
 
   render() {
     return (
       <Html>
-        <Head />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <Head>
+          <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="description" content="Land app for detailed land info." />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
-export default MyDocument
+export default MyDocument;
